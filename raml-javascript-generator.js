f5c9f438fd61eb015c17a64dc9960e7e26df4c10
Blake Embrey <hello@blakeembrey.com>
2015-06-16 20:05:41 +1000

    Dumb down fs and path usage for browserify

--- raml-javascript-generator.js
+++ raml-javascript-generator.js
@@ -1,20 +1,20 @@
-var join = require('path').join
-var readFileSync = require('fs').readFileSync
+var path = require('path')
+var fs = require('fs')
 var generator = require('raml-generator')
 
 module.exports = generator({
   templates: {
-    '.gitignore': readFileSync(join(__dirname, 'lib/templates/.gitignore.hbs'), 'utf8'),
-    'index.js': readFileSync(join(__dirname, 'lib/templates/index.js.hbs'), 'utf8'),
-    'README.md': readFileSync(join(__dirname, 'lib/templates/README.md.hbs'), 'utf8'),
-    'INSTALL.md': readFileSync(join(__dirname, 'lib/templates/INSTALL.md.hbs'), 'utf8'),
-    'package.json': readFileSync(join(__dirname, 'lib/templates/package.json.hbs'), 'utf8')
+    '.gitignore': fs.readFileSync(path.join(__dirname, 'lib/templates/.gitignore.hbs'), 'utf8'),
+    'index.js': fs.readFileSync(path.join(__dirname, 'lib/templates/index.js.hbs'), 'utf8'),
+    'README.md': fs.readFileSync(path.join(__dirname, 'lib/templates/README.md.hbs'), 'utf8'),
+    'INSTALL.md': fs.readFileSync(path.join(__dirname, 'lib/templates/INSTALL.md.hbs'), 'utf8'),
+    'package.json': fs.readFileSync(path.join(__dirname, 'lib/templates/package.json.hbs'), 'utf8')
   },
   partials: {
-    auth: readFileSync(join(__dirname, 'lib/partials/auth.js.hbs'), 'utf8'),
-    utils: readFileSync(join(__dirname, 'lib/partials/utils.js.hbs'), 'utf8'),
-    client: readFileSync(join(__dirname, 'lib/partials/client.js.hbs'), 'utf8'),
-    resources: readFileSync(join(__dirname, 'lib/partials/resources.js.hbs'), 'utf8')
+    auth: fs.readFileSync(path.join(__dirname, 'lib/partials/auth.js.hbs'), 'utf8'),
+    utils: fs.readFileSync(path.join(__dirname, 'lib/partials/utils.js.hbs'), 'utf8'),
+    client: fs.readFileSync(path.join(__dirname, 'lib/partials/client.js.hbs'), 'utf8'),
+    resources: fs.readFileSync(path.join(__dirname, 'lib/partials/resources.js.hbs'), 'utf8')
   },
   helpers: {
     stringify: require('javascript-stringify'),
